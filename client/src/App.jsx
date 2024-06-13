679ff761b9d241e5f4c79694b456d2458138ca02
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-06-13 13:29:01 +0530

    routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AdminLogin from './components/AdminLogin'
 import LandingPage from './components/LandingPage'
 import Navbar from './components/Navbar'
 import SuperAdminLogin from './components/SuperAdminLogin'
+import { Suspense } from "react"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <LandingPage />
-      <AdminLogin />
-      <SuperAdminLogin />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/adminlogin" element={<Suspense fallback="...loading"><AdminLogin /></Suspense> } />
+          <Route path="/superadminlogin" element={<Suspense fallback="...loading" ><SuperAdminLogin /></Suspense>} />
+          {/* <LandingPage />
+          <AdminLogin />
+          <SuperAdminLogin /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
