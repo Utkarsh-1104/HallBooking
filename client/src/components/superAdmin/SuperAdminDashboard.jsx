453e0683e8e08df838b55dc8a5baafd69709d1c9
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-08-19 09:57:33 +0530

    add admins ui

--- client/src/components/superAdmin/SuperAdminDashboard.jsx
+++ client/src/components/superAdmin/SuperAdminDashboard.jsx
@@ -1,8 +1,12 @@
+import AdminRegister from "./AdminRegister" 
 
 const SuperAdminDashboard = () => {
   return (
-    <div className='bg-black h-screen'>
+    <div className='bg-black h-screen font-[Roboto] flex flex-col items-center justify-center '>
+      <div>
         <h1 className='text-white text-center font-[Poppins] '>Super Admin Page</h1>
+        <AdminRegister />
+      </div>
     </div>
   )
 }
